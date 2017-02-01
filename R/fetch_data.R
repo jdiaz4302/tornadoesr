00b2512b9b769a4094f7c0bc2524eff7f49e5ca4
Max Joseph <maxwellbjoseph@gmail.com>
2017-02-01 11:21:52 -0700

    checking file name extraction and only downloading files if they don't already exist

--- R/fetch_data.R
+++ R/fetch_data.R
@@ -1,6 +1,4 @@
-library(RCurl)
-library(tidyverse)
-
+# Download storm event data from NOAA NWS ---------------------------------
 
 # Identify all of the files in the ftp directory --------------------------
 ftp_prefix <- "ftp://ftp.ncdc.noaa.gov/pub/data/swdi/stormevents/csvfiles/"
@@ -10,7 +8,15 @@ lines <- readLines("out.txt")
 # Find the filenames for storm detail csvs --------------------------------
 storm_detail_indices <- grep("StormEvents_details", lines)
 storm_lines <- lines[storm_detail_indices]
-filenames <- substring(storm_lines, 56)
+
+filename_start_index <- unlist(gregexpr("StormEvents", storm_lines))
+
+all_same_index <- length(unique(filename_start_index)) == 1
+if (!all_same_index) {
+  stop("The filenames begin at different indices in 'storm_lines'.")
+}
+
+filenames <- substring(storm_lines, unique(filename_start_index))
 
 
 # Generate full paths to the data files -----------------------------------
@@ -21,7 +27,9 @@ local_paths <- file.path(local_prefix, filenames)
 
 
 
-# Download all of the data files ------------------------------------------
+# Download all of the data files if they don't already exist ----------------
 for (i in seq_along(full_paths)) {
-  download.file(full_paths[i], local_paths[i])
+  if (!file.exists(local_paths[i])) {
+    download.file(full_paths[i], local_paths[i])
+  }
 }
