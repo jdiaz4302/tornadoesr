6107b18e43bc21051f428f3413aeaf5ca4754f78
Jeremy Diaz <jedi4302@colorado.edu>
2017-05-19 16:35:14 -0600

    Tidy'n up

--- R/fetch_data.R
+++ R/fetch_data.R
@@ -1,35 +1,73 @@
-# Download storm event data from NOAA NWS ---------------------------------
 
-# Identify all of the files in the ftp directory --------------------------
+
+
+# Set the Storm Events URL
 ftp_prefix <- "ftp://ftp.ncdc.noaa.gov/pub/data/swdi/stormevents/csvfiles/"
-download.file(ftp_prefix, destfile = "out.txt")
+
+
+# Download that page as a .txt
+download.file(ftp_prefix,
+              destfile = "out.txt")
+
+
+# Import all the lines from that .txt
 lines <- readLines("out.txt")
 
-# Find the filenames for storm detail csvs --------------------------------
-storm_detail_indices <- grep("StormEvents_details", lines)
+
+# Get all the line indices that contain relevant .csv's
+storm_detail_indices <- grep("StormEvents_details",
+                             lines)
+
+
+# Subset the total lines to only relevant .csv lines
 storm_lines <- lines[storm_detail_indices]
 
-filename_start_index <- unlist(gregexpr("/StormEvents", storm_lines))
 
+# Find where the .csv file name starts
+filename_start_index <- unlist(gregexpr(" StormEvents",
+                                        storm_lines))
+
+
+# Sets all_same_index to a T/F value, TRUE if only 1 index
 all_same_index <- length(unique(filename_start_index)) == 1
+
+
+# Throws error if they're not the same
 if (!all_same_index) {
   stop("The filenames begin at different indices in 'storm_lines'.")
-}
+  }
 
-filenames <- substring(storm_lines, unique(filename_start_index))
-filenames <- substring(filenames, 2, 52)
 
-# Generate full paths to the data files -----------------------------------
-full_paths <- paste0(ftp_prefix, filenames)
+# Subset the lines down to only the files that we wish to download
+filenames <- substring(storm_lines,
+                       unique(filename_start_index))
 
+
+# Gets rid of some empty space that exists due to an earlier
+# encountered problem
+filenames <- substring(filenames,
+                       2)
+
+
+# Generate full URL of the files
+full_paths <- paste0(ftp_prefix,
+                     filenames)
+
+
+# Where it's going in your directory
 local_prefix <- file.path("data", "raw")
+
+
+# Maintaining their original names in your directory
 local_paths <- file.path(local_prefix, filenames)
 
 
-
-# Download all of the data files if they don't already exist ----------------
+# Download all of the data files if they don't already exist
 for (i in seq_along(full_paths)) {
   if (!file.exists(local_paths[i])) {
-    download.file(full_paths[i], local_paths[i])
+    download.file(full_paths[i],
+                  local_paths[i])
+    }
   }
-}
+
+
--- R/merge_data.R
+++ R/merge_data.R
@@ -1,52 +1,70 @@
-# Merge all of the yearly storm event data --------------------------------
-library(tidyverse)
 
-# Find the most recent versions of all data files -------------------------
 
-# find all of the csv.gz files
+
+# Packages
+library(dplyr)
+library(readr)
+
+
+# Find all of the csv.gz files
 files <- list.files(path = file.path("data", "raw"), 
                     pattern = ".csv.gz", 
                     full.names = TRUE)
 
-# find out when the data files were last updated
-last_update_index <- gregexpr(pattern = "_c[:numbers:]*.", files) %>%
+
+# Get the index of where the file contains the last update date
+last_update_index <- gregexpr(pattern = "_c[:numbers:]*.",
+                              files) %>%
   unlist %>%
   unique
 
+
+# Get the update dates and format them appropriately
 file_updated <- substring(files, 
                           first = last_update_index + 2, 
                           last = last_update_index + 9) %>%
   as.Date(format = "%Y%m%d")
 
-# find out which year each data file is associated with
-year_index <- gregexpr(pattern = "_d[0-9]{4}", files) %>%
+
+# Get the index of where the file contains the .csv year
+year_index <- gregexpr(pattern = "_d[0-9]{4}",
+                       files) %>%
   unlist %>%
   unique
 
+
+# Get the .csv years
 file_year <- substring(files, 
                        first = year_index + 2, 
                        last = year_index + 5) %>%
   as.numeric
 
-# subset the data to only the most recent data files
-file_df <- tibble(files, file_updated, file_year) %>%
+# Subset the data to only the most recently updated data files
+# For each .csv / year
+file_df <- tibble(files,
+                  file_updated,
+                  file_year) %>%
   group_by(file_year) %>%
   mutate(most_recent_change = max(file_updated), 
          is_most_recent = file_updated == most_recent_change) %>%
   filter(is_most_recent)
 
-# delete old files that have been updated
-to_delete <- setdiff(files, file_df$files)
+
+# Delete old files that have been updated
+to_delete <- setdiff(files,
+                     file_df$files)
+
 file.remove(to_delete)
 
 
+# Merge the data
+tor_df <- lapply(files,
+                 read_csv,
+                 col_types = cols(DAMAGE_CROPS = "c",
+                                  EPISODE_ID = "c",
+                                  TOR_OTHER_CZ_FIPS = "c",
+                                  TOR_WIDTH = "d")) %>%
+  bind_rows() %>%
+  as.data.frame
 
 
-# Merging the data --------------------------------------------------------
-df <- lapply(files, 
-             read_csv, 
-             col_types = cols(DAMAGE_CROPS = "c", 
-                              EPISODE_ID = "c", 
-                              TOR_OTHER_CZ_FIPS = "c", 
-                              TOR_WIDTH = "d")) %>%
-  bind_rows() # bind the result together into one data frame
