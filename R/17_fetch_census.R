5dc92330a62efc710df6453c38dd31ccac51d4ab
Jeremy Diaz <jedi4302@colorado.edu>
2017-05-31 00:20:58 -0400

    Getting started on getting census data

---
+++ R/17_fetch_census.R
@@ -0,0 +1,18 @@
+
+
+
+# If needed
+# install.packages("tidyverse")
+# install.packages("devtools")
+# devtools::install_github("walkerke/tidycensus")
+
+
+# Packages
+library(tidycensus)
+library(tidyverse)
+library(xml2)
+
+
+# Set census api
+# Not going to include mine for Github
+census_api_key("")
