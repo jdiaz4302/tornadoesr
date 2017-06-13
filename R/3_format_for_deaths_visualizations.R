6facf32d33908c11989ddfbe1d02edb63c7ea928
Jeremy Diaz <jedi4302@colorado.edu>
2017-06-12 21:16:01 -0400

    Added code for injuries and deaths visualizations, they're drowning in zeros. Don't visualize well or easily.

---
+++ R/3_format_for_deaths_visualizations.R
@@ -0,0 +1,51 @@
+
+
+
+# Calling the data
+source("R/2_merge_data.R")
+
+
+# Filter to only tornadoes
+tor_df <- filter(tor_df,
+                 EVENT_TYPE == "Tornado")
+
+
+# Subsetting the dataset by date to ignore procedural changes
+tor_df <- filter(tor_df,
+                 YEAR >= 1997)
+
+
+# Limiting it to continental US - getting rid of messed up lat/long values
+tor_df <- filter(tor_df,
+                 BEGIN_LAT <= 50 & BEGIN_LAT >= 23)
+
+tor_df <- filter(tor_df,
+                 BEGIN_LON <= -66 & BEGIN_LON >= -125)
+
+
+# Formatting date
+tor_df$BEGIN_DATE_TIME <- as.POSIXct(tor_df$BEGIN_DATE_TIME,
+                                     format = "%d-%B-%y %H:%M:%S")
+
+
+# Fix years, such as 2050 should be 1950
+tor_df$BEGIN_DATE_TIME <- as.POSIXct(ifelse(tor_df$BEGIN_DATE_TIME >= "2049-12-31 23:59:59",
+                                            format(tor_df$BEGIN_DATE_TIME,
+                                                   "19%y-%m-%d %H:%M:%S"),
+                                            format(tor_df$BEGIN_DATE_TIME)))
+
+
+# Keeping only variables of interest
+tor_df <- dplyr::select(tor_df, c(DEATHS_DIRECT,
+                                  BEGIN_LAT,
+                                  BEGIN_LON,
+                                  BEGIN_DATE_TIME,
+                                  MONTH_NAME,
+                                  YEAR,                 
+                                  STATE))
+
+
+# Get rid of incomplete entries
+tor_df <- na.omit(tor_df)
+
+
