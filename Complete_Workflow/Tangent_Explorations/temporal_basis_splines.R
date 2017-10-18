a58cff3d72d627ad4628550fd95562b142dc0642
Jeremy Diaz <jedi4302@colorado.edu>
2017-10-18 14:50:10 -0600

    Updated and commented the basis spline exploration file

--- Complete_Workflow/Tangent_Explorations/temporal_basis_splines.R
+++ Complete_Workflow/Tangent_Explorations/temporal_basis_splines.R
@@ -25,6 +25,7 @@ tor_df <- read.csv('data/raw/tor_data_with_ACS.csv')
 # Get time as its own variable and in minutes from midnight
 tor_df$BEGIN_TIME <- as.POSIXct(tor_df$BEGIN_DATE_TIME, origin = '1970-01-01') %>%
   substring(12)
+
 tor_df$BEGIN_TIME <- substr(tor_df$BEGIN_TIME, 1, 2) %>%
   as.numeric() %>%
   `*`(60) %>%
@@ -44,37 +45,50 @@ tor_df$BEGIN_DATE <- (tor_df$BEGIN_DATE_TIME / 60) %>%
   floor()
 
 
-# Get a sequence of ordered days since 1970-01-01
+# Get a sequence for evenly spaced basis spline knots for date
 date_seq <- seq(from = min(tor_df$BEGIN_DATE),
                 to = max(tor_df$BEGIN_DATE),
                 by = 1)
-# Get the splines
-# MAYBE MAKE '4' A GENERALIZED CALCULATION
+
+
+# Get coarse splines
 coarse_date_splines <- bs(date_seq, 4, intercept = TRUE)
 
+
+# Get fine splines
 yearly_date_splines <- bs(date_seq, 20, intercept = TRUE)
 
 
-
-
-
-
+# Get a sequence for evenly spaced basis spline knots for time
 time_seq <- seq(from = min(tor_df$BEGIN_TIME),
                 to = max(tor_df$BEGIN_TIME),
                 by = 1)
+
+
+# Get coarse splines
 coarse_time_splines <- bs(time_seq, 4, intercept = TRUE)
+
+
+# Get fine splines
 hourly_time_splines <- bs(time_seq, 24, intercept = TRUE)
 
 
-
-
+# Get a sequence for evenly spaced basis spline knots for months
 julian_seq <- seq(from = 1,
                   to = 365,
                   by = 1)
+
+
+# Get monthly slines
 monthly_splines <- bs(julian_seq, 12, intercept = TRUE)
+
+
+# Plot the first monthly spline for inspection
 plot(julian_seq, monthly_splines[, 1], type = 'l',
      xlab = 'Day of the Year',
      ylab = 'Spline Height')
+
+# Add the other splines
 for (i in 2:ncol(monthly_splines)) {
   lines(julian_seq, monthly_splines[, i], col = i)
 }
@@ -82,7 +96,7 @@ for (i in 2:ncol(monthly_splines)) {
 
 
 
-# all date splines
+# Plot/inspect all date splines
 for (i in 1:ncol(coarse_date_splines)) {
   if (i == 1) {
     plot(date_seq, coarse_date_splines[, i], type = 'l',
@@ -92,6 +106,7 @@ for (i in 1:ncol(coarse_date_splines)) {
     lines(date_seq, coarse_date_splines[, i], col = i)
   }
 }
+
 for (i in 1:ncol(yearly_date_splines)) {
   lines(date_seq, yearly_date_splines[, i], col = i)
 }
@@ -99,7 +114,7 @@ for (i in 1:ncol(yearly_date_splines)) {
 
 
 
-# all time splines
+# Plot/inspect all time splines
 for (i in 1:ncol(coarse_time_splines)) {
   if (i == 1) {
     plot(time_seq, coarse_time_splines[, i], type = 'l',
@@ -109,66 +124,101 @@ for (i in 1:ncol(coarse_time_splines)) {
     lines(time_seq, coarse_time_splines[, i], col = i)
   }
 }
+
 for (i in 1:ncol(hourly_time_splines)) {
   lines(time_seq, hourly_time_splines[, i], col = i)
 }
 
 
-
-
-
-
-
-
-
-
-
+# Defining a function to convert the bs() results into dataframes
+# With usable columns
 bs_to_format_df <- function(col_prefix, bs_result) {
   
+  # bs_result = the output of bs()
+  # col_prefix = useful identifier for bs() columns
+  # Outputs a data.frame
+  
+  
+  # Convert the bs_result to a data.frame
   bs_df <- as.data.frame(bs_result)
   
+  # Get the prefix as a data.frame-length vector
   col_prefix_vector <- rep(col_prefix, length(colnames(bs_df)))
   
+  # Add the prefix to the column names, so they're not just integers
   colnames(bs_df) <- paste0(col_prefix_vector,
                             colnames(bs_df))
   
+  # Return the new, modified data.frame
   return(bs_df)
   
 }
 
 
-
+# Get the coarse date basis splines as a data.frame
 coarse_date_df <- bs_to_format_df('CD_SPLINE_', coarse_date_splines)
+
+# Get the fine data basis splines as a data.frame
 yearly_df <- bs_to_format_df('FD_SPLINE_', yearly_date_splines)
+
+# Combine these with the sequence as a reference key
 date_df <- cbind(date_seq,
                  coarse_date_df,
                  yearly_df)
+
+# Change the column name, more appropriate
 colnames(date_df)[1] <- 'BEGIN_DATE'
+
+
+# Same approach for monthly basis splines
 monthly_df <- bs_to_format_df('MONTH_SPLINE_', monthly_splines)
+
 monthly_df <- cbind(julian_seq,
                     monthly_df)
+
 colnames(monthly_df)[1] <- 'JULIAN_DAY'
+
+
+# Same approach for the time basis splines
 coarse_time_df <- bs_to_format_df('CT_SPLINE_', coarse_time_splines)
+
 hourly_df <- bs_to_format_df('FT_SPLINE_', hourly_time_splines)
+
 time_df <- cbind(time_seq,
                  coarse_time_df,
                  hourly_df)
+
 colnames(time_df)[1] <- 'BEGIN_TIME'
 
 
-
+# Use the 'reference key' data.frame to assign basis spline values
+# To the data set values
 tor_df <- merge(tor_df, monthly_df,
                 by = 'JULIAN_DAY')
+
 tor_df <- merge(tor_df, date_df,
                 by = 'BEGIN_DATE')
+
 tor_df <- merge(tor_df, time_df,
                 by = 'BEGIN_TIME')
 
-tor_df <- dplyr::select(tor_df,
-                        -c(BEGIN_TIME,
+
+# Get rid of and store the true temporal values for now
+# With EVENT_ID for later matching
+temp_df <- dplyr::select(tor_df,
+                         c(BEGIN_TIME,
                            BEGIN_DATE,
                            JULIAN_DAY,
                            EVENT_ID))
+
+
+# Move forward without the true temporal values
+tor_df <- dplyr::select(tor_df,
+                        -c(BEGIN_TIME,
+                           BEGIN_DATE,
+                           JULIAN_DAY))
+
+
 
 
 
@@ -401,6 +451,9 @@ tor_df <- dplyr::select(tor_df,
                            state_abbrev,
                            YEAR))
 
+tor_df <- merge(tor_df, temp_df, by = 'EVENT_ID') %>%
+  dplyr::select(-c(EVENT_ID))
+
 
 ###
 ###
@@ -465,12 +518,20 @@ cv_set <- tor_df[indicesValidation, ]
 zero_damage <- min(train_set$DAMAGE_PROPERTY)
 
 
+# Get rid of 0-property-damage events
 train_set <- dplyr::filter(train_set,
                            DAMAGE_PROPERTY > zero_damage)
+
 cv_set <- dplyr::filter(cv_set,
                         DAMAGE_PROPERTY > zero_damage)
 
+
+# Store the cv and train sets
+# Plus the reference key data.frames
 readr::write_csv(train_set, 'data/raw/spline_train.csv')
 readr::write_csv(cv_set, 'data/raw/spline_cv.csv')
+readr::write_csv(date_df, 'data/raw/date_spline_ref.csv')
+readr::write_csv(time_df, 'data/raw/time_spline_ref.csv')
+readr::write_csv(monthly_df, 'data/raw/monthly_spline_ref.csv')
 
 
