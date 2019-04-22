5a072f33939b5784e5567b3700400b9cf547d353
Jeremy Diaz <jedi4302@colorado.edu>
2019-04-22 03:11:26 +0000

    Completed prototyping of path-distribution-weighted extractions

--- Revisions/Explorations/weighted_LC_extractions_in_workflow.R
+++ Revisions/Explorations/weighted_LC_extractions_in_workflow.R
@@ -5,6 +5,8 @@
 library(readr)
 library(dplyr)
 library(raster)
+library(tidyr)
+library(plyr)
 install.packages('questionr')
 library(questionr)
 
@@ -81,14 +83,18 @@ tor_Spatial_2011 <- spTransform(tor_Spatial_2011,
 
 
 # We need the coordinates for the weighted extractions and Guassian template georef.
+# Keeping the EVENT_IDs too
 coords_2001 <- tor_Spatial_2001@coords
 coords_2001 <- coords_2001[1:(length(coords_2001) / 2), ]
+IDs_2001 <- tor_Spatial_2001@data$EVENT_ID
 
 coords_2006 <- tor_Spatial_2006@coords
 coords_2006 <- coords_2006[1:(length(coords_2006) / 2), ]
+IDs_2006 <- tor_Spatial_2006@data$EVENT_ID
 
 coords_2011 <- tor_Spatial_2011@coords
 coords_2011 <- coords_2011[1:(length(coords_2011) / 2), ]
+IDs_2011 <- tor_Spatial_2011@data$EVENT_ID
 
 
 # Defining function for the weighted extraction
@@ -118,8 +124,129 @@ weighted_extract <- function(coords) {
 }
 
 
-# Performing the weighted extraction
-wtd.table(weighted_extract(coords_2011[1, ]), weights = Gaussian_template)
-sum(wtd.table(weighted_extract(coords_2011[1, ]), weights = Gaussian_template))
+# Sitting up the data.frames for weighted extraction percentages
+NLCD_2001_factor_levels <- levels(NLCD_2001)
+NLCD_2001_factor_levels_filtered <- NLCD_2001_factor_levels[[1]][NLCD_2001_factor_levels[[1]]$COUNT != 0, ]$ID
+NLCD_2001_weighted_LC_prop <- matrix(ncol = length(NLCD_2001_factor_levels_filtered) + 1,
+                                     nrow = 0) %>%
+  data.frame()
+colnames(NLCD_2001_weighted_LC_prop) <- c(paste0('percent.', NLCD_2001_factor_levels_filtered),
+                                          'EVENT_ID')
+
+NLCD_2006_factor_levels <- levels(NLCD_2006)
+NLCD_2006_factor_levels_filtered <- NLCD_2006_factor_levels[[1]][NLCD_2006_factor_levels[[1]]$COUNT != 0, ]$ID
+NLCD_2006_weighted_LC_prop <- matrix(ncol = length(NLCD_2006_factor_levels_filtered) + 1,
+                                     nrow = 0) %>%
+  data.frame()
+colnames(NLCD_2006_weighted_LC_prop) <- c(paste0('percent.', NLCD_2006_factor_levels_filtered),
+                                          'EVENT_ID')
+
+NLCD_2011_factor_levels <- levels(NLCD_2011)
+NLCD_2011_factor_levels_filtered <- NLCD_2011_factor_levels[[1]][NLCD_2011_factor_levels[[1]]$COUNT != 0, ]$ID
+NLCD_2011_weighted_LC_prop <- matrix(ncol = length(NLCD_2011_factor_levels_filtered) + 1,
+                                     nrow = 0) %>%
+  data.frame()
+colnames(NLCD_2011_weighted_LC_prop) <- c(paste0('percent.', NLCD_2011_factor_levels_filtered),
+                                          'EVENT_ID')
+
+
+# Performing the weighted extractions and filling in data.frame rows
+for (i in 1:nrow(coords_2001)) {
+  weighted_extractions <- wtd.table(weighted_extract(coords_2001[i, ]), weights = Gaussian_template)
+  curr_df <- data.frame(weighted_extractions)
+  curr_df <- spread(curr_df, key = 'Var1', value = 'Freq', sep = "")
+  new_colnames <- c()
+  for (j in strsplit(colnames(curr_df), 'Var1')[]) {
+    new_colnames <- c(new_colnames, paste0('percent.', (j[2])))
+  }                             
+  colnames(curr_df) <- new_colnames
+  NLCD_2001_weighted_LC_prop <- rbind.fill(NLCD_2001_weighted_LC_prop, curr_df)
+  NLCD_2001_weighted_LC_prop[i, 'EVENT_ID'] <- IDs_2001[i]
+}
+
+for (i in 1:nrow(coords_2006)) {
+  weighted_extractions <- wtd.table(weighted_extract(coords_2006[i, ]), weights = Gaussian_template)
+  curr_df <- data.frame(weighted_extractions)
+  curr_df <- spread(curr_df, key = 'Var1', value = 'Freq', sep = "")
+  new_colnames <- c()
+  for (j in strsplit(colnames(curr_df), 'Var1')[]) {
+    new_colnames <- c(new_colnames, paste0('percent.', (j[2])))
+  }                             
+  colnames(curr_df) <- new_colnames
+  NLCD_2006_weighted_LC_prop <- rbind.fill(NLCD_2006_weighted_LC_prop, curr_df)
+  NLCD_2006_weighted_LC_prop[i, 'EVENT_ID'] <- IDs_2006[i]
+}
+
+for (i in 1:nrow(coords_2011)) {
+  weighted_extractions <- wtd.table(weighted_extract(coords_2011[i, ]), weights = Gaussian_template)
+  curr_df <- data.frame(weighted_extractions)
+  curr_df <- spread(curr_df, key = 'Var1', value = 'Freq', sep = "")
+  new_colnames <- c()
+  for (j in strsplit(colnames(curr_df), 'Var1')[]) {
+    new_colnames <- c(new_colnames, paste0('percent.', (j[2])))
+  }                             
+  colnames(curr_df) <- new_colnames
+  NLCD_2011_weighted_LC_prop <- rbind.fill(NLCD_2011_weighted_LC_prop, curr_df)
+  NLCD_2011_weighted_LC_prop[i, 'EVENT_ID'] <- IDs_2011[i]
+}
+
+
+# Put them all together
+LC_df <- rbind(NLCD_2001_weighted_LC_prop,
+               NLCD_2006_weighted_LC_prop,
+               NLCD_2011_weighted_LC_prop)
+
+
+# NA's really mean 0, "missing land cover" = "none of that land cover"
+LC_df[is.na(LC_df)] <- 0
+
+
+# Classification value of 0 is not valid, so git rid of that as well
+LC_df <- dplyr::select(LC_df,
+                       -percent.0)
+
+
+# Merge the LC values with the tornado events
+tor_LC_df <- merge(x = tor_df,
+                   y = LC_df,
+                   by = 'EVENT_ID')
+
+
+# Give the LC values a name
+colnames(tor_LC_df)[colnames(tor_LC_df) == "percent.11"] <- "OPEN_WATER_PROP"
+
+colnames(tor_LC_df)[colnames(tor_LC_df) == "percent.12"] <- "ICE_SNOW_PROP"
+
+colnames(tor_LC_df)[colnames(tor_LC_df) == "percent.21"] <- "DEV_OPEN_PROP"
+
+colnames(tor_LC_df)[colnames(tor_LC_df) == "percent.22"] <- "DEV_LOW_PROP"
+
+colnames(tor_LC_df)[colnames(tor_LC_df) == "percent.23"] <- "DEV_MED_PROP"
+
+colnames(tor_LC_df)[colnames(tor_LC_df) == "percent.24"] <- "DEV_HIGH_PROP"
+
+colnames(tor_LC_df)[colnames(tor_LC_df) == "percent.41"] <- "DECID_FOREST_PROP"
+
+colnames(tor_LC_df)[colnames(tor_LC_df) == "percent.42"] <- "EVERGR_FOREST_PROP"
+
+colnames(tor_LC_df)[colnames(tor_LC_df) == "percent.43"] <- "MIXED_FOREST_PROP"
+
+colnames(tor_LC_df)[colnames(tor_LC_df) == "percent.52"] <- "SHRUB_SCRUB_PROP"
+
+colnames(tor_LC_df)[colnames(tor_LC_df) == "percent.71"] <- "GRASS_LAND_PROP"
+
+colnames(tor_LC_df)[colnames(tor_LC_df) == "percent.81"] <- "PASTURE_HAY_PROP"
+
+colnames(tor_LC_df)[colnames(tor_LC_df) == "percent.82"] <- "CULT_CROPS_PROP"
+
+colnames(tor_LC_df)[colnames(tor_LC_df) == "percent.90"] <- "WOOD_WETLAND_PROP"
+
+colnames(tor_LC_df)[colnames(tor_LC_df) == "percent.95"] <- "HERB_WETLAND_PROP"
+
+colnames(tor_LC_df)[colnames(tor_LC_df) == "percent.31"] <- "BARREN_LAND_PROP"
+
+
+# Then save the dataframe as .csv
+# readr::write_csv(tor_LC_df, "data/raw/tor_data_with_NLCD.csv")
 
 
